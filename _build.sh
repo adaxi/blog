188fc7a931a29a957e0a1f798f73d22cbeb4d1bc
adaxi <dev@adaxisoft.be>
2016-10-19 21:27:53 +0200

    Add key to ssh-agent before cloning repository

--- _build.sh
+++ _build.sh
@@ -13,6 +13,16 @@ fi
 
 REPOSITORY="git@github.com:${TRAVIS_REPO_SLUG}"
 
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy.key.enc -out deploy.key -d
+chmod 600 deploy.key
+eval `ssh-agent -s`
+ssh-add deploy.key
+
 git clone $REPOSITORY build-area
 cd ./build-area
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
@@ -35,16 +45,6 @@ fi
 
 git add .
 git commit -m "Deploy to branch ${TRAVIS_BRANCH}: ${TRAVIS_COMMIT}"
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy.key.enc -out deploy.key -d
-chmod 600 deploy.key
-eval `ssh-agent -s`
-ssh-add deploy.key
 
 # Now that we're all set up, we can push.
 git push $REPOSITORY $TARGET_BRANCH
